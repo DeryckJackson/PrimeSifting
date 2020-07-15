11523b3a20f5c5668ae8dd29716befdbd9827a1f
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-07-15 09:37:25 -0700

    intial commit with empty files
    Co-Authored-By: Micheal Hansen <micheal.hansen@gmail.com>

---
+++ PrimeSifting.Solution/Primes.Tests/PrimesTests/PrimesTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Blank;
+
+namespace Blank.Tests
+{
+  [TestClass]
+  public class BlankTests
+  {
+    [TestMethod]
+    // Test method will go here
+  }
+}
